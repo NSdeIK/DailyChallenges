7400a8a39dff4522526c52c0006362b8c2ef6c4c
NSdeIK <71137188+NSdeIK@users.noreply.github.com>
2022-11-08 23:16:31 +0100

    Üres projekt létrehozása

---
+++ app/src/main/java/hu/inf/unideb/dailychallenges/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.inf.unideb.dailychallenges
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
